ceca8fae857cb3039889d4ee1572592e5fc5123b
cmseibel <cam.m.seib@gmail.com>
2021-11-29 21:25:51 -0800

    Write timer functionality, start modal implementation.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var timerEl = document.querySelector(".timer");
 
-var today=moment();
-timerEl.innerHTML=today.format("[Current date and time] MMM Do, YYYY [at] h:mm:ss a[.]");
+var timer = setInterval(function(){
+    var today = moment();
+    timerEl.innerHTML=today.format("[Current date and time] MMM Do, YYYY [at] h:mm:ss a[.]");
+}, 1000)
 
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
